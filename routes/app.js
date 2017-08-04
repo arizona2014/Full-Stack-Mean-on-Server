e609ee0da9efbe7fedab8ae7e3b8b9511715a914
arizona2014 <andy.lisac@gmail.com>
2017-08-04 23:46:50 +0200

    Cleaned the code

--- routes/app.js
+++ routes/app.js
@@ -1,24 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+
 /*
-router.get('/', function (req, res, next) {
-    res.render('index');
-});
+    router.get('/', function (req, res, next) {
+        res.render('index');
+    });
 
 
-router.get('/json', function (req, res, next) {
-    res.json({"first_name": "andy","last_name": "lisac"});
-});
+    router.get('/json', function (req, res, next) {
+        res.json({"first_name": "andy","last_name": "lisac"});
+    });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {message: req.params.msg});
-});
+    router.get('/message/:msg', function (req, res, next) {
+        res.render('node', {message: req.params.msg});
+    });
 
-router.post('/message', function(req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
-});
+    router.post('/message', function(req, res, next) {
+        var message = req.body.message;
+        res.redirect('/message/' + message);
+    });
 */
 
 router.get('/', function (req, res, next) {
