bf113a56f1da7b59642382abc62687b2cf94e348
arizona2014 <andy.lisac@gmail.com>
2017-10-30 16:38:02 +0100

    user model added

---
+++ assets/app/user.model.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Arizona on 10/30/2017.
+ */
+export class User {
+    constructor(public email:string, public password: string, public firstName?: string, public lastName?: string){
+
+    }
+}
