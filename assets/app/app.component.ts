2e2061159106ba9bac3a32b6ab1de4b680ae9a48
arizona2014 <andy.lisac@gmail.com>
2017-10-30 09:28:41 +0100

    and some styles too

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [`
+        .author {
+            display: inline-block;
+            font-style: italic;
+            font-size: 12px;
+            width: 80%;            
+        }
+        .config{
+            display: inline-block;
+            text-align: right;
+            font-size: 12px;
+            width: 19%;                
+        }
+    `]
 })
 export class AppComponent {
-    content = "Some content";
+
+    message = {
+        content : "A message",
+        author : "Andy"
+    }
+
 }
