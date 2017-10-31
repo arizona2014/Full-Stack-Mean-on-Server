4cc43241527480cbcaabd00576c5a77b8a4719ba
arizona2014 <andy.lisac@gmail.com>
2017-10-31 09:17:02 +0100

    removed subcomponent css from app

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,21 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    styles: [`
-        .author {
-            display: inline-block;
-            font-style: italic;
-            font-size: 12px;
-            width: 80%;            
-        }
-        .config{
-            display: inline-block;
-            text-align: right;
-            font-size: 12px;
-            width: 19%;                
-        }
-    `]
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
 
