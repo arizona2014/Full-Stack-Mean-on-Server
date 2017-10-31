d10f4b9b6be2910b240ff89e20b42f4ac3c6bf00
arizona2014 <andy.lisac@gmail.com>
2017-10-31 09:18:12 +0100

    inserted message component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
+import { MessageComponent } from "./messages/message.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MessageComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
