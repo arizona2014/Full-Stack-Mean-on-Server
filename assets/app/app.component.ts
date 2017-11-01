c8a9dd1d4196628823b29c048ad67a09e92a0d69
arizona2014 <andy.lisac@gmail.com>
2017-11-01 07:23:37 +0100

    refacto for message list component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-
-    message = {
-        content : "A message",
-        author : "Andy"
-    }
 
 }
