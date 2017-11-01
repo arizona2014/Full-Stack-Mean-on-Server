081df3d4fcbe6efb681e458bfb8bc7c2e54100f1
arizona2014 <andy.lisac@gmail.com>
2017-11-01 07:23:54 +0100

    add message list component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { MessageComponent } from "./messages/message.component";
+import { MessageListComponent } from "./messages/message-list.component";
+import {MessageInputComponent} from "./messages/message-input.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
