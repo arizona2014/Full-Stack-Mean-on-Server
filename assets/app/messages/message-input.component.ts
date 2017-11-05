322500d3c33b9e8da1fb7ee8b1396c6a5eff5ade
arizona2014 <andy.lisac@gmail.com>
2017-11-05 12:54:50 +0100

    imported message model

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {MessageService} from "./messages.service";
+import {Message} from "./message.model";
 
 @Component({
     selector: 'app-message-input',
