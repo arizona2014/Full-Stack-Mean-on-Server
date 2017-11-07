405f54cf2d0b41f235acfd6c7e1354fafeddaa55
arizona2014 <andy.lisac@gmail.com>
2017-11-07 09:30:53 +0100

    inserting new components

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,13 +6,19 @@ import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import {MessageInputComponent} from "./messages/message-input.component";
 import {MessageService} from "./messages/messages.service";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
+import {HeaderComponent} from "./header.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
