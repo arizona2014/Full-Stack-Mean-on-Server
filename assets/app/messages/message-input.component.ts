f0d772f7b58d786685c63ce0fc8de48ef182500b
arizona2014 <andy.lisac@gmail.com>
2017-11-07 08:59:01 +0100

    removing message service from this component

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -4,8 +4,7 @@ import {Message} from "./message.model";
 
 @Component({
     selector: 'app-message-input',
-    templateUrl: './message-input.component.html',
-    providers: [MessageService]
+    templateUrl: './message-input.component.html'
 })
 
 export class MessageInputComponent {
