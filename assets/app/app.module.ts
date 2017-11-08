d12d156f6a161f8089f7e06e0b14920431d614de
arizona2014 <andy.lisac@gmail.com>
2017-11-08 08:40:27 +0100

    presenting auth sub components

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
-import {MessageInputComponent} from "./messages/message-input.component";
-import {MessageService} from "./messages/messages.service";
-import {MessagesComponent} from "./messages/messages.component";
-import {AuthenticationComponent} from "./auth/authentication.component";
-import {HeaderComponent} from "./header.component";
+import { MessageInputComponent } from "./messages/message-input.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { AuthenticationComponent } from "./auth/authentication.component";
+import { HeaderComponent } from "./header.component";
+import { routing } from "./app.routing";
+import { LogoutComponent } from "./auth/logout.component";
+import { SigninComponent } from "./auth/signin.component";
+import { SignupComponent } from "./auth/signup.component";
 
 @NgModule({
     declarations: [
@@ -18,9 +21,12 @@ import {HeaderComponent} from "./header.component";
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
-        HeaderComponent
+        HeaderComponent,
+        LogoutComponent,
+        SigninComponent,
+        SignupComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
