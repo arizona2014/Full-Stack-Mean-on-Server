66eb786e246951a3790040b9a39d2b7a7706e4d9
arizona2014 <andy.lisac@gmail.com>
2017-11-08 08:41:59 +0100

    accomodating auth sub components

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -1,7 +1,20 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'app-authentication',
-    template: `<h1>Authentication</h1>`
+    template: `
+        <header class="row spacing">
+            <nav class="col-md-8 col-md-offset-2">
+                <ul class="nav nav-tabs">
+                    <li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>
+                    <li routerLinkActive="active"><a [routerLink]="['signin']">Signin</a></li>                    
+                    <li routerLinkActive="active"><a [routerLink]="['logout']">Logout</a></li>
+                </ul>    
+            </nav>
+        </header>
+        <div class="row spacing">
+            <router-outlet></router-outlet>
+        </div>
+    `
 })
 export class AuthenticationComponent {
 
