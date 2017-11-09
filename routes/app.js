de0c50f65daf67ea724e4bd7bdfe3379bad70609
arizona2014 <andy.lisac@gmail.com>
2017-11-09 07:42:45 +0100

    presenting message route

--- routes/app.js
+++ routes/app.js
@@ -2,54 +2,11 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-/*
-    router.get('/', function (req, res, next) {
-        res.render('index');
-    });
-
-
-    router.get('/json', function (req, res, next) {
-        res.json({"first_name": "andy","last_name": "lisac"});
-    });
-
-    router.get('/message/:msg', function (req, res, next) {
-        res.render('node', {message: req.params.msg});
-    });
-
-    router.post('/message', function(req, res, next) {
-        var message = req.body.message;
-        res.redirect('/message/' + message);
-    });
-*/
-
 router.get('/', function (req, res, next) {
-
-// Commented for adding Angular
-/*    User.findOne({email: 'yasmi@gmail.com'}, function(err, doc){
-        if(err){
-            return res.send('Error');
-        }
-        res.render('message', {email:doc});
-    });*/
-// Commented for adding Angular
 
     res.render('index');
 
 });
 
-
-// Commented for adding Angular
-/*router.post('/', function(req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Max',
-        lastName: 'Swhwarz',
-        password: 'secret',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
-});*/
-// Commented for adding Angular
 
 module.exports = router;
