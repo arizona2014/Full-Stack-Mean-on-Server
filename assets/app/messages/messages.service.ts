079c25fc1c97314fdf1e56e73b48262018eafefb
arizona2014 <andy.lisac@gmail.com>
2017-11-09 07:45:27 +0100

    crud operations with messages

--- assets/app/messages/messages.service.ts
+++ assets/app/messages/messages.service.ts
@@ -1,21 +1,62 @@
 import { Message } from './message.model';
+import { Http, Response, Headers } from "@angular/http";
+import { Injectable, EventEmitter } from "@angular/core";
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
 
+@Injectable()
 export class MessageService {
 
     private messages : Message[] = [];
+    messageIsEdit = new EventEmitter<Message>();
+
+    constructor(private http: Http){ }
 
     addMessage(message: Message){
-        this.messages.push(message);
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/message', body, {headers: headers})
+            .map((response: Response) => {
+                const result = response.json();
+                const message = new Message(result.obj.content, 'Dummy', result.obj._id, null );
+                this.messages.push(message);
+                return message;
+            })
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 
     getMessages(){
-        return this.messages;
+        return this.http.get('http://localhost:3000/message')
+            .map((response: Response) => {
+                const messages = response.json().obj;
+                let transformedMessages: Message[] = [];
+                for (let message of messages){
+                    transformedMessages.push(new Message(message.content, message._id, 'Dummy', null));
+                }
+                this.messages = transformedMessages;
+                return transformedMessages;
+            })
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 
     deleteMessage(message: Message){
         this.messages.splice(this.messages.indexOf(message),1);
+        return this.http.delete('http://localhost:3000/message/' + message.messageId)
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 
+    editMessage(message: Message){
+        this.messageIsEdit.emit(message);
+    }
+
+    updateMessage(message: Message){
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
 
 }
 
