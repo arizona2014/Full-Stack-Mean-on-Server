a80947c8e9d085ba3df2b73753a4383599eb7595
arizona2014 <andy.lisac@gmail.com>
2017-11-09 07:42:34 +0100

    presenting message route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
+var messageRoutes = require('./routes/messages');
 
 var app = express();
 mongoose.connect('localhost:27017/node-angular');
@@ -28,6 +29,7 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use('/message', messageRoutes);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
