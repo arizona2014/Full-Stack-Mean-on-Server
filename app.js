29f6e3ba281aeaae7300b309fc72230ddfca97e5
arizona2014 <andy.lisac@gmail.com>
2017-11-10 07:48:02 +0100

    presenting user route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
 var messageRoutes = require('./routes/messages');
+var userRoutes = require('./routes/user');
 
 var app = express();
 mongoose.connect('localhost:27017/node-angular');
@@ -30,6 +31,7 @@ app.use(function (req, res, next) {
 });
 
 app.use('/message', messageRoutes);
+app.use('/user', userRoutes);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
