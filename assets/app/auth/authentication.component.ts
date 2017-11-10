6f9cfedb21a21ac5de8053a1d951f0fee46a34e9
arizona2014 <andy.lisac@gmail.com>
2017-11-10 07:50:34 +0100

    isLogged function

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {AuthService} from "./auth.service";
 @Component({
     selector: 'app-authentication',
     template: `
@@ -6,8 +7,8 @@ import {Component} from "@angular/core";
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-tabs">
                     <li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>
-                    <li routerLinkActive="active"><a [routerLink]="['signin']">Signin</a></li>                    
-                    <li routerLinkActive="active"><a [routerLink]="['logout']">Logout</a></li>
+                    <li routerLinkActive="active" *ngIf="!isLoggedIn()"><a [routerLink]="['signin']">Signin</a></li>                    
+                    <li routerLinkActive="active" *ngIf="isLoggedIn()"><a [routerLink]="['logout']">Logout</a></li>
                 </ul>    
             </nav>
         </header>
@@ -17,5 +18,11 @@ import {Component} from "@angular/core";
     `
 })
 export class AuthenticationComponent {
+
+    constructor(private authService: AuthService){ }
+
+    isLoggedIn(){
+        return this.authService.isLoggedIn();
+    }
 
 }
