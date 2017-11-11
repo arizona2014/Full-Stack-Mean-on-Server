5907556d336a69b93559d4d541e40355244151f3
arizona2014 <andy.lisac@gmail.com>
2017-11-11 17:33:59 +0100

    displaying operations only to valid user

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -36,4 +36,9 @@ export class MessageComponent {
                 result => console.log(result)
             );
     }
+
+    belongsToUser(){
+        return localStorage.getItem('userId') == this.message.userId;
+    }
+
 }
