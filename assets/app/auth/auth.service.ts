91a424f91538d8aa3955000bd13fdf173ae1e7c3
arizona2014 <andy.lisac@gmail.com>
2017-11-12 02:44:14 +0100

    error component & service accomodating

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -3,18 +3,22 @@ import { Injectable } from "@angular/core";
 import {Http, RequestOptions} from "@angular/http";
 import 'rxjs/Rx';
 import { Observable } from "rxjs";
+import { ErrorService } from "../errors/error.service";
 
 @Injectable()
 export class AuthService {
 
-    constructor(private http: Http) { }
+    constructor(private http: Http, private errorService: ErrorService) { }
 
     signup(user: User) {
         const body = user;
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('http://localhost:3000/user', body, {headers: headers})
             .map((response: Response) => response.json())
-            .catch((error: Response) => Observable.throw(error.json()));
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json())
+            });
     }
 
     signin(user: User) {
@@ -22,7 +26,10 @@ export class AuthService {
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('http://localhost:3000/user/signin', body, {headers: headers} )
             .map((response: Response) => response.json())
-            .catch((error: Response) => Observable.throw(error.json()));
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json());
+            });
     }
 
     logout(){
