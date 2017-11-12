a0e4bf5018df7b426f0e041547007af9e8ab37dc
arizona2014 <andy.lisac@gmail.com>
2017-11-12 02:43:21 +0100

    signin comp update

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,5 +1,8 @@
 import {Component} from "@angular/core";
 import {FormGroup, Validators, FormControl} from "@angular/forms";
+import {User} from "./user.model";
+import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
@@ -7,8 +10,22 @@ import {FormGroup, Validators, FormControl} from "@angular/forms";
 export class SigninComponent {
     myForm: FormGroup;
 
+    constructor(private authService: AuthService, private router: Router){ }
+
     onSubmit(){
-        console.log(this.myForm);
+        const user = new User(this.myForm.value.email, this.myForm.value.password);
+        this.authService.signin(user)
+            .subscribe(
+                ( data ) => {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('userId', data.userId);
+                    this.router.navigateByUrl('/');
+
+                },
+                ( error ) => {
+                    console.error(error)
+                }
+            )
         this.myForm.reset();
     }
 
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,5 +1,8 @@
-import {Component, OnInit} from "@angular/core";
-import {FormGroup, FormControl, Validators} from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "./auth.service";
+import { User } from "./user.model";
+
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html'
@@ -8,8 +11,24 @@ export class SignupComponent implements OnInit {
 
     myForm: FormGroup;
 
+    constructor(private authService: AuthService){ }
+
     onSubmit(){
-        console.log(this.myForm);
+        const user = new User(
+            this.myForm.value.email,
+            this.myForm.value.password,
+            this.myForm.value.firstName,
+            this.myForm.value.lastName
+        );
+        this.authService.signup(user)
+            .subscribe(
+                ( data ) => {
+                    console.log(data)
+                },
+                ( error ) => {
+                    console.error(error)
+                }
+            )
         this.myForm.reset();
     }
 
